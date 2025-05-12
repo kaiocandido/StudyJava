f4cd7c4f36613b4ad55a3305f6caa4b391c2764d
kaio candido <kaio.candf@gmail.com>
2025-05-12 18:34:48 -0300

    Using the methods with overload

--- metodos2.java
+++ metodos2.java
@@ -1,4 +1,4 @@
-public class Metodos2 {
+public class metodos2 {
     
     static double hipotenusa(double cateto1, double cateto2){
         double temp1 = Math.pow(cateto1, 2);
@@ -8,9 +8,25 @@ public class Metodos2 {
         return  temp4;
     }
 
+    static void hipotenusa(String name){
+        System.out.println("O seu nome e: " + name);
+    }
+
+    static void hipotenusa(){
+        int sum = 2 + 4;
+        System.out.println("A soma dos numero e: " + sum);
+    }
+
     public static void main(String[] args) {
         double result = hipotenusa(4, 8);
         System.out.println(result);
+
+
+        hipotenusa("Leila");
+        
+        hipotenusa();
+        
+
     }
 
 
