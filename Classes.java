8c0657d5a69f009dfeacb61269057d9db9e726b4
kaio candido <kaio.candf@gmail.com>
2025-05-13 22:36:53 -0300

    add methodos with set and get

--- Classes.java
+++ Classes.java
@@ -2,14 +2,14 @@ public class Classes {
     
     public static void main(String[] args) {
         Conta c1 = new Conta();
-        c1.numero = 3232;
-        c1.nome = "Kaio";
+        c1.setNumero(3232);
+        c1.setName("Kaio");
 
-        System.out.println("Dados da conta: " + c1.nome + " N: " + c1.numero + " Saldo: " + c1.getSaldo());
+        System.out.println("Dados da conta: " + c1.getName() + " N: " + c1.getNumero() + " Saldo: " + c1.getSaldo());
         System.out.println("Realizando testes");
         c1.retirar(8);
         c1.depositar(100);
-        System.out.println("Dados da conta: " + c1.nome + " N: " + c1.numero + " Saldo: " + c1.getSaldo());
+        System.out.println("Dados da conta: " + c1.getName() + " N: " + c1.getNumero() + " Saldo: " + c1.getSaldo());
 
     }
 
--- Conta.java
+++ Conta.java
@@ -1,21 +1,37 @@
 public class Conta {
-    public int numero;
-    public String nome;
+    private  int numero;
+    private  String nome;
     private double saldo = 0;
 
-    public void depositar (double v){
+    public int getNumero(){
+        return numero;
+    }
+    public void setNumero(int x){
+        numero = x;
+    }
+
+    public String getName(){
+        return nome;
+    }
+
+    public void setName(String x){
+        nome = x;
+    }
+
+    public double getSaldo(){
+        return saldo;
+    }
+
+        public void depositar (double v){
         saldo += v;
     }
 
+    
     public void retirar (double x){
         if (saldo >= x) {
             saldo -= x;
         }else{
             System.out.println("Saldo Insuficiente");
         }
-    }
-
-    public double getSaldo(){
-        return saldo;
     }
 }
