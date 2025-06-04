2776abd9980ad22f0d7b92f3d950521082887f00
kaio candido <kaio.candf@gmail.com>
2025-06-03 23:23:58 -0300

    study date and hours in java

---
+++ modulo2/Ultilitarias/dataHrs.java
@@ -0,0 +1,16 @@
+/*
+ *  MANIPULANDO DATA E HORAS
+    
+    Antiga API
+    * Classe java.ultil.Date - Representa uma data e hora, mas é considerada obsoleta.
+    * CLasse java.text.SimpleDateFormat - Permite formatar e analisar datas em strings.
+
+    Nova API
+    * Classe java.time.LocalDate - Representa uma data (sem hora).
+    * Classe  java.tome.format.DateTimeFormatter - Permite formatar e analisar datas em strings.
+
+ */
+
+public class dataHrs {
+    
+}
