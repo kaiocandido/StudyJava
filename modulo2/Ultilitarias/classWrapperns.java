c65cf1ad8cf0bc552e8347f304d5c1b55e60f9b0
kaio candido <kaio.candf@gmail.com>
2025-06-03 22:49:17 -0300

    class wrapperns in java

---
+++ modulo2/Ultilitarias/classWrapperns.java
@@ -0,0 +1,15 @@
+/*
+ *  Possuem metodos estaticos ultilizados na conversão de string
+
+    CLASSE JAVA.LANG.OBJECT
+    
+    *  principal classe da linguagem java
+    *  é a classe mãe de todas as outras classes
+    *  Possui metodos de base que são automaticmaente herdados por todas as classes que existem
+    *  Dentre os principais metodos herdados podemos citar o metodo toString() que pode
+    ser sobrescrito para retornar uma representação em string do objeto.
+ */
+
+public class classWrapperns {
+    
+}
